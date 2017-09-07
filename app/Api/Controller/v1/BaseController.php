7479535fdb96f6c947e07cea3b0e5d1ee6f5680f
wangqingxian <736713830@qq.com>
2017-09-07 16:46:29 +0800

    asd

---
+++ app/Api/Controller/v1/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Api\Controller\V1;
+use App\Http\Controllers\Controller;
+use Dingo\Api\Routing\Helpers;
+
+class BaseController extends Controller
+{
+    use Helpers;
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', ['namespace' => 'App\Api\Controller\V1'], function ($api) {
+    $api->group(['middleware' => 'api.auth'],function($api){
+        $api->get('home', 'HomeController@index');
+        $api->get('save', 'HomeController@save');
+    });
+
+    // 授权组
+    $api->group(['prefix' => 'auth'], function ($api) {
+        $api->get('register', 'AuthenticateController@register')->name('auth.register');
+        $api->get('authenticate', 'AuthenticateController@authenticate')->name('auth.authenticate');
+        $api->get('logout', 'AuthenticateController@logout')->name('auth.logout');
+        $api->get('refresh', 'AuthenticateController@refresh')->name('auth.refresh');
+    });
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
